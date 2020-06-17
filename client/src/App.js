bae7c4920c773aa8bf268e922a9f63e98074558b
EndePointe <alvin.johnsii@gmail.com>
2020-06-17 16:27:00 -0700

    ssdd

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+
+  return (
+  <div>
+    <button onClick={showStripe}>stripe</button> 
+  </div>
+  );
+}
+
+export default App;
---
+++ server/store.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const stripe = require('stripe')('', {apiVersion: ''});
+const app = express();
+const port = 3000;
+
+
+
+app.get('/', (req,res) => {
+const paymentIntent = await stripe.paymentIntents.create({
+  amount: 1099,
+  currency: 'usd',
+  // Verify your integration in this guide by including this parameter
+  metadata: {integration_check: 'accept_a_payment'},
+});
+  console.log(stripe);
+});
+
+
+app.listen(port, () => console.log('store running'));
