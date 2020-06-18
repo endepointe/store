be38f0b6e15f9a36f2be6fa075ec2a78f9986417
EndePointe <endepointe@gmail.com>
2020-06-18 08:31:45 +0100

    ssdd

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,103 @@
-import React from 'react';
+import React,
+{
+  useState
+} from 'react';
+import {
+  Elements,
+  CardElement,
+  useElements,
+  useStripe
+} from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+// Custom styling can be passed to options when creating an Element.
+const CARD_ELEMENT_OPTIONS = {
+  style: {
+    base: {
+      width: '200px',
+      color: '#32325d',
+      fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+      fontSmoothing: 'antialiased',
+      fontSize: '16px',
+      '::placeholder': {
+        color: '#aab7c4'
+      }
+    },
+    invalid: {
+      color: '#fa755a',
+      iconColor: '#fa755a'
+    }
+  }
+};
+
+const CheckoutForm = () => {
+  const [error, setError] = useState(null);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  // Handle real-time validation errors from the card Element.
+  const handleChange = (event) => {
+    if (event.error) {
+      setError(event.error.message);
+    } else {
+      setError(null);
+    }
+  }
+
+  // Handle form submission.
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const card = elements.getElement(CardElement);
+    const result = await stripe.createToken(card)
+    if (result.error) {
+      // Inform the user if there was an error.
+      setError(result.error.message);
+    } else {
+      setError(null);
+      // Send the token to your server.
+      stripeTokenHandler(result.token);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-row">
+        <label for="card-element">
+          Credit or debit card
+        </label>
+        <CardElement
+          id="card-element"
+          options={CARD_ELEMENT_OPTIONS}
+          onChange={handleChange}
+        />
+        <div className="card-errors" role="alert">{error}</div>
+      </div>
+      <button type="submit">Submit Payment</button>
+    </form>
+  );
+}
+const stripePromise = loadStripe("");
 
 const App = () => {
 
   return (
-  <div>
-    <button onClick={showStripe}>stripe</button> 
-  </div>
+    <Elements stripe={stripePromise}>
+      <CheckoutForm />
+    </Elements>
   );
+}
+
+// POST the token ID to your backend.
+async function stripeTokenHandler(token) {
+  const response = await fetch('/charge', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ token: token.id })
+  });
+
+  return response.json();
 }
 
 export default App;
--- server/store.js
+++ server/store.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const stripe = require('stripe')('', {apiVersion: ''});
+const stripe = require('stripe')('', { apiVersion: '' });
 const app = express();
 const port = 3000;
 
 
 
-app.get('/', (req,res) => {
-const paymentIntent = await stripe.paymentIntents.create({
-  amount: 1099,
-  currency: 'usd',
-  // Verify your integration in this guide by including this parameter
-  metadata: {integration_check: 'accept_a_payment'},
-});
+app.get('/charge', (req, res) => {
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: 1099,
+    currency: 'usd',
+    // Verify your integration in this guide by including this parameter
+    metadata: { integration_check: 'accept_a_payment' },
+  });
   console.log(stripe);
 });
 
