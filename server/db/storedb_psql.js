2f75fff041285273a75f234c0d77c36493774be0
EndePointe <endepointe@gmail.com>
2020-06-25 21:54:55 +0100

    ssdd

--- server/db/storedb_psql.js
+++ server/db/storedb_psql.js
@@ -1,6 +1,13 @@
 const pgp = require('pg-promise')();
-const db = pgp(`${process.env.PSQLUSER}://${process.env.PSQLUSER}:${process.env.PSQLPASS}@localhost:5432/store`);
-
-console.log(db);
+const cn = {
+  host: 'localhost',
+  port: 5432,
+  database: 'store',
+  user: process.env.PSQLUSER,
+  password: process.env.PSQLPASS,
+  max: 30 // use up to 30 connections
+}
+const db = pgp(cn);
+// const db = pgp(`${process.env.PSQLUSER}://${process.env.PSQLUSER}:${process.env.PSQLPASS}@localhost:5432/store`);
 
 module.exports = { db };
