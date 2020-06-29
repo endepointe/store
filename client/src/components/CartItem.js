c18fee4a545bfc4b5cb9740d6c9c1f5f03ae3f27
EndePointe <endepointe@gmail.com>
2020-06-29 23:29:21 +0100

    addrm btns to for cart modal

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import { removeProduct } from '../redux/actions';
+import { addProduct, removeProduct } from '../redux/actions';
 import { connect } from 'react-redux';
+import RemoveIcon from '@material-ui/icons/Remove';
+import AddIcon from '@material-ui/icons/Add';
 
 const useStyles = makeStyles({
   item: {
     width: '100%',
     display: 'flex',
+    alignItems: 'center',
     listStyle: 'none',
     flexDirection: 'row',
     marginBotton: '1rem',
     marginTop: '1rem',
-    paddingTop: '0.2rem',
-    paddingBottom: '0.2rem',
     backgroundColor: '#ededed',
   },
   name: {
@@ -23,27 +24,55 @@ const useStyles = makeStyles({
   price: {
     flex: 1,
     textAlign: 'right',
-    marginRight: '2rem',
+    marginRight: '.5rem',
+  },
+  removeButton: {
+    width: '1.5rem',
+    border: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    color: 'red',
+  },
+  addButton: {
+    width: '1.5rem',
+    border: 'none',
+    display: 'flex',
+    justifyContent: 'center',
+    color: 'green',
   }
 });
 
 const CartItem = (props) => {
   const classes = useStyles();
 
+  const addItem = () => {
+    props.addProduct(props.item.content);
+  }
+
   const removeItem = () => {
     console.log(props.item);
     props.removeProduct(props.item);
   }
+
   return (
     <li className={classes.item}>
       <span className={classes.name}>{props.item.content.name}</span>
       <span className={classes.price}>$ {props.item.content.price}</span>
-      <button onClick={removeItem}>remove</button>
+      <button
+        className={classes.addButton}
+        onClick={addItem}>
+        <AddIcon />
+      </button>
+      <button
+        className={classes.removeButton}
+        onClick={removeItem}>
+        <RemoveIcon />
+      </button>
     </li>
   );
 }
 
 export default connect(
   null,
-  { removeProduct }
+  { addProduct, removeProduct }
 )(CartItem);
